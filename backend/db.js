502cac15733603a59f63dcad4cf9db5a340ae862
suhel-24 <21pa1a05g3@vishnu.edu.in>
2024-03-05 00:38:42 +0530

    done

---
+++ backend/db.js
@@ -0,0 +1,55 @@
+// backend/db.js
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.hrft8xq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log('MongoDB connection successful'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+  Account,
+};
